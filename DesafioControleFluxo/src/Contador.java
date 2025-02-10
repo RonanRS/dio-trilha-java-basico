bd58477c8e2afab59ce5a1fb76fb9d91e10c1015
Ronan Ramalho <ronan.souza5@gmail.com>
2025-02-10 07:44:30 +0000

    Adicionado o DesafioControleFluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Contador {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = scan.nextInt();
 
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = scan.nextInt();
 
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro precisa ser maior do que o primeiro.");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm < parametroDois) {
+            int contagem = parametroDois - parametroUm;
+            for (int i = 0; i < contagem; i++) {
+                System.out.println("Imprimindo o número " + (i + 1));
+            }
+        } else {
+            throw new ParametrosInvalidosException();
+        }
     }
 }
